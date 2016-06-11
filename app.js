e634dcb35e91ae3d070d69198ea6fe6b3711da13
Sahat Yalkabov <sakhat@gmail.com>
2016-06-11 03:53:37 -0400

    do not use dotenv

--- app.js
+++ app.js
@@ -20,11 +20,6 @@ const multer = require('multer');
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
 /**
- * Load environment variables from .env file, where API keys and passwords are configured.
- */
-dotenv.load({ path: '.env.example' });
-
-/**
  * Controllers (route handlers).
  */
 const homeController = require('./controllers/home');
