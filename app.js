32daf6add29f75b2c4bdf04a561db87b24e86223
Vuyani M. Shabangu <vuyani.shabangu@gmail.com>
2016-06-14 11:56:36 +0200

    Configuring mongo

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ const app = express();
 /**
  * Connect to MongoDB.
  */
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connect(/*process.env.MONGODB_URI || */process.env.MONGOLAB_URI);
 mongoose.connection.on('error', () => {
   console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
   process.exit(1);
